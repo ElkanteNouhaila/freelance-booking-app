52ad40c550989eacf0c16a30e1948e76716ce315
Nouhaila El Kante <nouhailaelkante2@gmail.com>
2026-02-22 12:32:54 +0000

    fix: enhance login functionality and error handling
    - Added check for undefined JWT_SECRET in the login function, returning a 500 error if not set.
    - Updated the login response to include user details along with the token for improved client-side handling.
    - Changed the success message from 'Sign in successful' to 'Login successful' for consistency.

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -69,11 +69,29 @@ async function login(req, res) {
     if (!validpass) {
       return res.status(401).json({ error: 'Invalid password' });
     }
-    const JWT_SECRET = process.env.JWT_SECRET;
 
-    const token = jwt.sign({id: user._id , role: user.role}, JWT_SECRET, { expiresIn: '24h' });
+    const secretkey = process.env.JWT_SECRET;
+    if(!secretkey){
+      console.error("JWT_SRCRET is not defined in .env")
+      return res.status(500).json({message:"Internal server error"})
+    }
 
-    res.status(200).json({ message: 'Sign in successful', token });
+    const token = jwt.sign(
+      {id: user._id , role: user.role}, 
+    secretkey, 
+    { expiresIn: '24h' }
+  );
+
+    res.status(200).json({ 
+      message: 'Login successful', 
+      token,
+      user:{
+        id:user._id,
+        name:user.name,
+        email:user.email,
+        role:user.role
+      }
+    });
 
   } catch (error) {
       console.error(error);
